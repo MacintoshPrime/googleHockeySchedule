7cfe58775643a1d2a7493cf7cdbf6c9173e080bc
carty <chris.carty@gmail.com>
2013-11-08 15:34:37 +0000

    now works on nhl.com.

--- schedule.py
+++ schedule.py
@@ -1,39 +1,60 @@
 from bs4 import BeautifulSoup as Soup
 import urllib2
 
+def nhl(cells):
+    '''
+    Assume cell order is date/team/team2. As found on NHL.com
+    '''
+    date = cells[0].get_text()
+    team1 = cells[1].get_text()
+    team2 = cells[2].get_text()
+    return [date + ' ' + team1 + ' vs ' + team2]
+	
+def other(cells):
+    '''
+    Assume cell order is date/time/location/teamname/teamname
+    '''
+    date = cells[0].get_text()
+    time = cells[1].get_text()
+    location = cells[2].get_text()
+    team1 = cells[3].get_text()
+    team2 = cells[4].get_text()
+    return [date + ' ' + time + ' ' + location + ' ' + team1 + ' vs ' + team2]
+
 #testing update
 def schedule(url, teamname):
     '''
     Takes an url and a teamname.
     Retruns teams schedule
     '''
-
-
     html = urllib2.urlopen(url)
 
     soup = Soup(html)
 
     tables = soup.find_all("table")
     
-    for i in tables:
-        print "new Table"
-        print "\n"
-        print i.find('tbody')
-        #if i.find('tbody').find_all('tr') < 0:
-         #   searchtable = i.find('tbody').find_all('tr')
-
-    return
     upcoming = []
-
-    for row in searchtable:
-	    cells = row.find_all('td')
-	    for matches in cells:
-	        if teamname in matches:
-	            date = cells[0].get_text()
-	            time = cells[1].get_text()
-	            location = cells[2].get_text()
-	            team1 = cells[3].get_text()
-	            team2 = cells[4].get_text()
-	            upcoming.append( [date + ' ' + time + ' ' + location + ' ' + team1 + ' vs ' + team2])
-	
+    
+    '''
+    Loop through tables on page.
+    Find any that contain the tbody tag.
+    If there is a tbody tag loop through the rows and columns to find a cell that contain the team name.
+    Once found get data from the row and return it.
+    '''
+    for i in tables:
+        if i.find('tbody') in i:
+            for x in i.find('tbody').find_all('tr'):
+                cells = x.find_all('td')
+                for matches in cells:
+                    #print 'new: ' + matches.get_text()
+                    if teamname in matches.get_text():
+                        
+                        #if url contain nhl use the nhl parser
+                        if 'nhl' in url:
+                            upcoming.append(nhl(cells))
+                        #otherwise use the standard parser.    
+                        else:
+                            upcoming.append(other(cells))
+	       
     return upcoming
+
